24776245c5d29376a79b84ac7d6510837fb9bd40
James Murphy <jmurphy4@mail.umw.edu>
2017-02-21 15:39:05 -0500

    mg_obj restructure

--- mg_obj.cpp
+++ mg_obj.cpp
@@ -1,52 +1,41 @@
 // Write code for creating and destroying mg_objects
-#include <iostream>
 #include <string>
 #include <map>
 #include <iostream>
 #include "mg_obj.h"
 #include "tree.h"
 
-mg_obj * mg_alloc_int(int value) {
+mg_obj * mg_alloc(obj_type o, void * value, std::string name_of_type) {
 	mg_obj * m = new mg_obj;
-	m->type = INT;
-	m->value = new int(value);
-	return m;
-}
-
-mg_obj * mg_alloc_flt(double value) {
-	mg_obj * m = new mg_obj;
-	m->type = FLT;
-	m->value = new double(value);
-	return m;
-}
-
-mg_obj * mg_alloc_str(std::string s) {
-	mg_obj * m = new mg_obj;
-	m->type = STR;
-	m->value = new std::string(s);
-	return m;
-}
-
-mg_obj * mg_alloc_func(node * v) {
-	mg_obj * m = new mg_obj;
-	m->type = FUNC;
-	m->value = new node;
-	*(m->value) = *v;
-	return m;
-}
-
-mg_obj * mg_alloc_meth(node * v) {
-	mg_obj * m = new mg_obj;
-	m->type = METH;
-	m->value = new node;
-	*(m->value) = *v;
+	switch (o) {
+		case FUNC:
+			break;
+		
+		case METH:
+			break;
+		
+		case INT:
+			m->type = INT;
+			m->value = new int(*value);
+			break;
+		
+		case FLT:
+			m->type = FLT;
+			m->value = new double(*value);
+			break;
+		
+		case STR:
+			m->type = STR;
+			m->value = new std::string(*value);
+			break;
+	}
 	return m;
 }
 
 // Allocate memory for a user-defined type instance using the name of the
 // type, the names of each member of an instance of this type, and the
 // values corresponding to each member.
-mg_obj * mg_alloc_type(
+mg_format * mg_alloc_type(
 	std::string name_of_type,
 	std::string members[],
 	mg_obj values[]
