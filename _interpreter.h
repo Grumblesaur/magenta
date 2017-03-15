f68527f9ce4dc2bba94de141b68b0985eae4f773
Taylor Dohmen <tdohmen@mail.umw.edu>
2017-03-15 17:35:36 -0400

    reference files

---
+++ _interpreter.h
@@ -0,0 +1,14 @@
+#ifndef INTERP_H
+#define INTERP_H
+
+#include "tree.h"
+#include "mg_obj.h"
+
+#ifdef __cplusplus
+extern "C" {
+	mg_obj* eval_expr(struct node* node);
+	void eval_stmt(struct node* node);
+};
+#endif
+
+#endif
