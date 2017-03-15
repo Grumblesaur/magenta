86e9929725b7f896116595261f1059b4e803b234
James Murphy <jmurphy4@mail.umw.edu>
2017-03-15 13:15:28 -0400

    overhaul in progress

--- interpreter.cpp
+++ interpreter.cpp
@@ -7,13 +7,37 @@
 #include "tree.h"
 #include "mg_obj.h"
 
+using std::string;
+using std::cout;
+using std::endl;
+
 template <typename T>
 std::unordered_map<std::string, mg_obj<T>> vars;
 
 
 template <typename T>
 mg_obj<T> eval_expr(struct node * node) {
+	bool t_val;
+	mg_obj<T> left, right;
 
+	switch (node->token) {
+		case IDENTIFIER:
+			return vars[string((char *)node->value)];
+		case INTEGER_LITERAL:
+			return mg_obj<int>(TYPE_INTEGER, *(int *)node->value);
+		case FLOAT_LITERAL:
+			return mg_obj<double>(TYPE_FLOAT, *(double *)node->value);
+		case STRING_LITERAL:
+			return mg_obj<string>(TYPE_STRING, string((char *)node->value);
+		case PAREN_OPEN:
+			return eval_expr(node->children[0]);
+		case LOG_NOT:
+			right = eval_expr(node->children[0]);
+			t_val = !eval_bool(right);
+			return mg_obj<int>(TYPE_INTEGER, new int(t_val));
+		default:
+			cout << "STILL WORKING ON IT" << endl;
+	}
 	return mg_obj<int>(TYPE_INTEGER);
 }
 
