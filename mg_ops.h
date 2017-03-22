2f7fffcb303666cdb5dd2e2d187769c82b640739
James Murphy <jmurphy4@mail.umw.edu>
2017-03-22 12:23:34 -0400

    sorry about that

---
+++ mg_ops.h
@@ -0,0 +1,21 @@
+#ifndef MG_OPS
+#define MG_OPS
+
+#include "mg_types.h"
+
+using std::string;
+
+mg_obj * power(mg_obj * left, mg_obj * right);
+mg_obj * logarithm(mg_obj * left, mg_obj * right);
+mg_obj * multiply(mg_obj * left, mg_obj * right);
+mg_obj * divide(mg_obj * left, mg_obj * right);
+mg_obj * mod(mg_obj * left, mg_obj * right);
+mg_obj * add(mg_obj * left, mg_obj * right);
+mg_obj * subtract(mg_obj * left, mg_obj * right);
+mg_obj * eval_logical(mg_obj * left, int token, mg_obj * right);
+bool eval_comp(mg_obj * left, int op, mg_obj * right);
+int eval_bitwise(mg_obj * left, int token, mg_obj * right);
+bool eval_bool(mg_obj * o);
+
+
+#endif
